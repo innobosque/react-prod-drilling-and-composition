c09a70b0dd2fe2a2ed482744a871177bad0582a8
Xurxo González <xgonten.innobosque.gmail.com>
2022-12-02 16:51:15 +0100

    Realizados pruebas con test
    -describe()
    -toThrow()
    -toBeNaN()

--- __test__/prod_drilling/demo.test.js
+++ __test__/prod_drilling/demo.test.js
@@ -1,18 +1,42 @@
-test('Esta prueba no debe de fallar',()=>{
-    //inicialización
-    //estímulo
-    //aserción --> observar el comportamiento ... esperado
-})
-
-test('La constante "saludo" no debe de tener espacios en blanco',()=>{
-    //inicialización
-    const saludo = 'Hola mundo';
-
-    //estímulo
-    const saludo2 = saludo.trim();
-
-    //aserción
-    expect(saludo).toBe(saludo2);
-    expect(saludo2).toBeTruthy();
-    expect(Number(saludo2)).toBeNaN();//Comprobamos que es NaN
+describe('Pruebas en demo', () => {
+    const CP = {
+        valid: '15401',
+        invalid: '1',
+        invalidText: 'Código postal no válido'
+    }
+    
+    const codigoPostal =  texto => {
+        if(!/^[0-9]{5}$/.test(texto = texto.trim()))
+          throw new Error(`${CP.invalidText} [${texto}]`);
+        return texto;
+    }
+    
+    test('Esta prueba no debe de fallar',()=>{
+        //inicialización
+        //estímulo
+        //aserción --> observar el comportamiento ... esperado
+    })
+    
+    test(`Validar código ${CP.valid}`,()=>{
+        expect(codigoPostal(CP.valid)).toBe(CP.valid)
+    })
+    
+    test('Validar si el código es inválido',()=>{
+        //Aserción
+        expect(()=>codigoPostal(CP.invalid)).toThrow(`${CP.invalidText} [${CP.invalid}]`);
+        
+    })
+    
+    test('La constante "saludo" no debe de tener espacios en blanco',()=>{
+        //inicialización
+        const saludo = 'Hola mundo';
+    
+        //estímulo
+        const saludo2 = saludo.trim();
+    
+        //aserción
+        expect(saludo).toBe(saludo2);
+        expect(saludo2).toBeTruthy();
+        expect(Number(saludo2)).toBeNaN();//Comprobamos que es NaN
+    })
 })
